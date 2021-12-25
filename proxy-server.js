2e2f345cb7c9f6202ec88f9134ddab6b09018c7b
priyang patel <priyang@priyangs-MacBook-Air.local>
2021-12-25 17:06:31 +0530

    Network Error

--- proxy-server.js
+++ proxy-server.js
@@ -1,21 +1,21 @@
-const express = require('express');
-const request = require('request');
-const path = require('path');
+const express = require("express");
+const request = require("request");
+const path = require("path");
 const app = express();
-var cors = require('cors');
+var cors = require("cors");
 
 app.use(
   cors({
-    origin: '*',
+    origin: "*",
   })
 );
-app.get('/api', (req, res) => {
+app.get("/api", (req, res) => {
   console.log(req.query.ApiCall);
   request(
     { url: `http://numbersapi.com/${req.query.ApiCall}` },
     (error, response, body) => {
       if (error || response.statusCode !== 200) {
-        return res.status(500).json({ type: 'error', message: error.message });
+        return res.status(500).json({ type: "error", message: error.message });
       }
       res.json(body);
     }
@@ -25,19 +25,19 @@ app.get('/api', (req, res) => {
 //static for Browser
 const _dirname = path.resolve();
 
-app.use('/Photos', express.static(path.join(__dirname, '/Photos')));
+app.use("/Photos", express.static(path.join(__dirname, "/Photos")));
 
-if (process.env.NODE_ENV === 'production') {
-  app.use(express.static(path.join(_dirname, '/client/build')));
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(path.join(_dirname, "/client/build")));
 
-  app.get('*', (req, res) =>
-    res.sendFile(path.resolve(_dirname, 'client', 'build', 'index.html'))
+  app.get("*", (req, res) =>
+    res.sendFile(path.resolve(_dirname, "client", "build", "index.html"))
   );
 } else {
-  app.get('/', (req, res) => {
-    res.send('API is running....');
+  app.get("/", (req, res) => {
+    res.send("API is running....");
   });
 }
 
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 5001;
 app.listen(PORT, () => console.log(`listening on ${PORT}`));
