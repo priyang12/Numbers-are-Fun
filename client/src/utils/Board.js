1eb2fb72ff5b8e74d44704df7c13333857e4255e
priyang patel <priyang@priyangs-MacBook-Air.local>
2021-12-25 20:12:56 +0530

    Added Icon and Chnage Name

--- client/src/utils/Board.js
+++ client/src/utils/Board.js
@@ -1,7 +1,7 @@
 const Board = (props) => {
   const { type } = props;
   return (
-    <section id={`blackboard-${type || 'primary'}`} className='container'>
+    <section id={`blackboard-${type || "primary"}`} className='container board'>
       {props.children}
     </section>
   );
